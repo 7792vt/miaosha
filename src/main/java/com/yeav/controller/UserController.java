829a0d0a13c3288ec5f23abeb5cf47144075370b
yeav <983266335@qq.com>
2022-09-19 14:24:18 +0800

    opt验证码的获取

--- src/main/java/com/yeav/controller/UserController.java
+++ src/main/java/com/yeav/controller/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * @author yeav
@@ -25,6 +26,30 @@ public class UserController extends BaseController {
 
     @Autowired(required = false)
     private UserService userService;
+
+    @Autowired(required = false)
+    private HttpServletRequest httpServletRequest;
+
+    //用户获取otp短信接口
+    @RequestMapping("/getotp")
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name = "telphone") String telphone) {
+        //需要按照一定规则生成opt验证码
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);
+        randomInt += 10000;
+        String otpCode = String.valueOf(randomInt);
+
+        //将opt验证码同对应用户的手机号关联,使用httpseesion的方式绑定
+        httpServletRequest.getSession().setAttribute(telphone, otpCode);
+
+        //将otp验证码通过短信通道发送给用户，省略
+        System.out.println("telphone = " + telphone + " & otpCode = " + otpCode);
+
+
+        return CommonReturnType.create(null);
+    }
+
 
     @RequestMapping("/get")
     @ResponseBody
